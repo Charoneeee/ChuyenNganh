37dc66e1d5493373dda678c457b2e6a86ee9ebc1
Sang Nguyen <131627842+Charoneeee@users.noreply.github.com>
2023-12-11 00:19:30 +0700

    Create Contact

--- figmar_app_1/pages/index.tsx
+++ figmar_app_1/pages/index.tsx
@@ -14,6 +14,7 @@ import {
   Button,
   CardContent,
   CardMedia,
+  TextField,
 } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -495,6 +496,136 @@ export default function Home() {
             }}
           >
             See all review
+          </Button>
+        </Grid>
+        <Grid className="About">
+          <Typography
+            style={{
+              position: "absolute",
+              left: "620px",
+              top: "7550px",
+              fontSize: "81px",
+              fontFamily: "Playfair Display, serif",
+            }}
+          >
+            Contact
+          </Typography>
+          <Grid
+            style={{
+              position: "absolute",
+              left: "329px",
+              top: "7700px",
+              fontSize: "81px",
+              fontFamily: "Playfair Display, serif",
+            }}
+          >
+            <Typography
+              style={{
+                fontSize: "55px",
+                fontFamily: "Inter, sans-serif",
+              }}
+            >
+              Name
+            </Typography>
+            <TextField
+              id="filled-textarea"
+              placeholder="John Doe"
+              multiline
+              variant="filled"
+              style={{
+                width: "855px",
+                borderBottom: "groove",
+              }}
+              InputProps={{
+                style: {
+                  color: "white", // Đặt màu chữ thành màu trắng
+                  fontSize: "25px",
+                },
+              }}
+            />
+          </Grid>
+          <Grid
+            style={{
+              position: "absolute",
+              left: "329px",
+              top: "7900px",
+              fontSize: "81px",
+              fontFamily: "Playfair Display, serif",
+            }}
+          >
+            <Typography
+              style={{
+                fontSize: "55px",
+                fontFamily: "Inter, sans-serif",
+              }}
+            >
+              Email
+            </Typography>
+            <TextField
+              id="filled-textarea"
+              placeholder="jhon@email.com"
+              multiline
+              variant="filled"
+              style={{
+                width: "855px",
+                borderBottom: "groove",
+              }}
+              InputProps={{
+                style: {
+                  color: "white", // Đặt màu chữ thành màu trắng
+                  fontSize: "25px",
+                },
+              }}
+            />
+          </Grid>
+          <Grid
+            style={{
+              position: "absolute",
+              left: "329px",
+              top: "8100px",
+              fontSize: "81px",
+              fontFamily: "Playfair Display, serif",
+            }}
+          >
+            <Typography
+              style={{
+                fontSize: "55px",
+                fontFamily: "Inter, sans-serif",
+              }}
+            >
+              Message
+            </Typography>
+            <TextField
+              id="standard-multiline-static"
+              multiline
+              rows={4}
+              placeholder="Enter your message here..."
+              variant="standard"
+              style={{
+                width: "855px",
+                borderBottom: "groove",
+              }}
+              InputProps={{
+                style: {
+                  color: "white", // Đặt màu chữ thành màu trắng
+                  fontSize: "25px",
+                },
+              }}
+            />
+          </Grid>
+          <Button
+            variant="contained"
+            style={{
+              position: "absolute",
+              left: "327px",
+              top: "8450px",
+              fontSize: "38px",
+              fontFamily: "Inter, sans-serif",
+              backgroundColor: "white",
+              color: "black",
+            }}
+          >
+            Send
           </Button>
         </Grid>
       </main>
